682c8c97ceede0eea5f320cb33bd0cebb561403a
Gabriel Miklós <socketbind@gmail.com>
2020-02-09 23:08:18 +0100

    Tweak look and configuration.

--- js/app.js
+++ js/app.js
@@ -7,17 +7,29 @@ import { ReactReader, ReactReaderStyle } from "react-reader";
 
 import mobyDick from '../moby-dick.epub';
 
-console.log(mobyDick);
-
 const customStyle = Object.assign({}, ReactReaderStyle, {
     container: {
+        ...ReactReaderStyle.container,
         overflow: 'hidden',
         width: '100%',
         height: '100%'
+    },
+    readerArea: {
+        ...ReactReaderStyle.readerArea,
+        background: '#f4ddb9'
     }
 });
 
 class App extends React.Component {
+    getRendition = (rendition) => {
+        rendition.themes.default({
+            body: {
+                background: '#f4ddb9',
+                color: '#000'
+            }
+        });
+    };
+
     render() {
         return <ReactReader
             className="reader"
@@ -28,6 +40,7 @@ class App extends React.Component {
             styles={customStyle}
             location={localStorage.getItem('epubLocation')}
             locationChanged={epubcifi => localStorage.setItem('epubLocation', epubcifi)}
+            getRendition={this.getRendition}
         />
     }
 }
