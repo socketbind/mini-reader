c115f72665803662e8160fcffee3c6f6e66d6e83
Gabriel Miklós <socketbind@gmail.com>
2020-04-13 18:25:32 +0200

    Make mobile safari happy (SVG issues).

--- frontend/js/loading.js
+++ frontend/js/loading.js
@@ -3,6 +3,14 @@ import loading from '../svg/loading.svg';
 
 export class Loading extends React.Component {
   render() {
-    return <div className="system-background"><img src={loading} width="100%" height="100%" /></div>;
+    return <div className="system-background">
+      <object
+          type="image/svg+xml"
+          width="100%"
+          height="100%"
+          alt="Startup animation"
+          data={loading}>
+      </object>
+    </div>;
   }
 }
